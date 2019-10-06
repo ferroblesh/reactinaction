ef63fb002c61ae6dbb078860bea55a75e68b5533
Fernando Robles <ferroblesh@gmail.com>
2019-10-05 20:14:13 -0500

    Component Types

---
+++ src/components/Stateful.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Stateful extends Component {
+    render() {
+        return (
+            <h1>Hola mundo</h1>
+        )
+    }
+}
---
+++ src/components/Stateless.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Stateless = () => {
+    return (
+        <h1>Hola Mundo</h1>
+    );
+};
+
+export default Stateles;
